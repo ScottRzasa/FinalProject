fbf3e29f59332f341b58f21399dd9c64616bd792
ScottRzasa <arzasa@students.kennesaw.edu>
2024-11-20 15:04:20 -0500

    more nav

--- fp/src/Navbar.jsx
+++ fp/src/Navbar.jsx
@@ -6,9 +6,9 @@ function Navbar(){
 
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div className="container-fluid">
+            <div className="container-fluid collapse navbar-collapse">
             <a className="navbar-brand" href="#">Volkswagen Microsite</a>
-                <nav>
+                <nav className="mb-2 mb-lg-0">
                     <Link to="/">Home</Link> |
                     <Link to="/history">History</Link> |
                     <Link to="/gallery">Gallery</Link> |
