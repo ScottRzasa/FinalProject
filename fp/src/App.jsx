985f6dd10292ac00ffb1c733f6ff5259b2dc150b
ScottRzasa <arzasa@students.kennesaw.edu>
2024-11-19 19:20:45 -0500

    update

--- fp/src/App.jsx
+++ fp/src/App.jsx
@@ -3,13 +3,29 @@ import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
 import Home from "./Home.jsx";
 import About from "./About.jsx";
 import History from ".History.jsx";
-import Gallery from ".Gallery";
-import Resources from "./Resources";
+import Gallery from ".Gallery.jsx";
+import Resources from "./Resources.jsx";
+import Navbar from "./Navbar.jsx";
+import Footer from "./Footer.jsx";
 
 function App (){
 
   return (
-    <></>
+
+    <Router>
+      <Navbar></Navbar>
+      <About></About>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/history" element={<History />} />
+      <Route path="/gallery" element={<Gallery />} />
+      <Route path="/resources" element={<Resources />} />
+    </Routes>
+    <Footer></Footer>
+    </Router>
+
+
+
   )
 }
 
