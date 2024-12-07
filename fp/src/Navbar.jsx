d034acf341011486b428558b88470ebef53f6bf5
ScottRzasa <arzasa@students.kennesaw.edu>
2024-12-06 23:49:46 -0500

    logo

--- fp/src/Navbar.jsx
+++ fp/src/Navbar.jsx
@@ -6,8 +6,14 @@ function Navbar(){
 
     return(
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a className="navbar-brand" href="#">
+              <div className="logo-container">
+                <img className="logo" src="Volkswagen images/3dlogo4.png" alt="Volkswagen Logo" />
+              </div>
+              Volkswagen Microsite
+            </a>
             <div className="container-fluid collapse navbar-collapse">
-            <a className="navbar-brand " href="#">Volkswagen Microsite</a>
+            <a className="navbar-brand"></a>
                 <nav className="mb-2 mb-lg-0">
                     <Link to="/">Home</Link> |
                     <Link to="/about">About</Link> |
