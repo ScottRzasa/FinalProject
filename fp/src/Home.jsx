832c26897c3274f82f3c3822f2250dd67a84a1f9
ScottRzasa <arzasa@students.kennesaw.edu>
2024-12-09 11:30:36 -0500

    home

--- fp/src/Home.jsx
+++ fp/src/Home.jsx
@@ -9,9 +9,10 @@ function Home(){
             <div className="row">
                 <div className="col-md-6">
                     <h1>Welcome to Volkswagen</h1>
-                    <p>Discover the legacy of Volkswagen, a brand that has shaped automotive history
-                        with iconic cars like the Beetle, Golf, and their performance in motorsport
-                        events worldwide.</p>
+                    <p>Discover the legacy of Volkswagen, a brand that has signified its place in automotive history.
+                        With the creation of iconic cars such as the Beetle, Golf, Jetta, and an extensive collection
+                        of other vehicles, Volkswagen has made itself a household name.
+                    </p>
                 </div>
                 <div className="col-md-6">
                     <img src="Volkswagen images/DSC-0069.avif" alt="Volkswagen Rally Car" />
